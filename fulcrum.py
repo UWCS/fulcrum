874da723d46a7067672695c9c8133a1cc66a4c26
Joel Coulon <Joel.Coulon@warwick.ac.uk>
2025-06-02 18:36:04 +0100

    refactored auth

--- fulcrum.py
+++ fulcrum.py
@@ -1,58 +1,24 @@
 import os
 from pathlib import Path
-from typing import Any
 
-from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
-from flask import Flask, redirect, render_template, session, url_for
-from werkzeug.wrappers import Response
+from flask import Flask, render_template
+
+from auth.auth import auth_bp, configure_oauth, is_exec, is_logged_in
 
 # if .env file exists, load it
 if Path(".env").exists():
     load_dotenv()
 
+# initialise flask app
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 
-oath = OAuth(app)
-oath.register(
-    name="keycloak",
-    client_id="events",
-    client_secret=os.getenv("KEYCLOAK_CLIENT_SECRET"),
-    server_metadata_url="https://auth.uwcs.co.uk/realms/uwcs/.well-known/openid-configuration",
-    client_kwargs={
-        "scope": "openid profile groups",
-        "token_endpoint_auth_method": "client_secret_post",
-    },
-)
+# setup oauth and add routes
+configure_oauth(app)
+app.register_blueprint(auth_bp)
 
 
 @app.route("/")
 def index() -> str:
-    return render_template(
-        "index.html", name=session.get("name"), groups=session.get("groups")
-    )
-
-
-@app.route("/login")
-def login() -> Any:  # noqa: ANN401
-    redirect_uri = url_for("auth", _external=True)
-    return oath.keycloak.authorize_redirect(redirect_uri)  # type: ignore
-
-
-@app.route("/auth")
-def auth() -> Response:
-    token = oath.keycloak.authorize_access_token()  # type: ignore
-    user = token["userinfo"]  # type: ignore
-    session["name"] = user.get("name", "Unknown")
-    session["groups"] = user.get("groups", [])
-    return redirect(url_for("index"))
-
-
-@app.route("/logout")
-def logout() -> Response:
-    session.clear()
-    return redirect(
-        "https://auth.uwcs.co.uk/realms/uwcs/protocol/openid-connect/logout?redirect_uri="
-        + url_for("index", _external=True)
-    )
+    return render_template("index.html", is_logged_in=is_logged_in(), is_exec=is_exec())
